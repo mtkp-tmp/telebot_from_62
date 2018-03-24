e49b6c9b89bb78c3ee01c57d71802230f1800e1e
AlexOdlin <rockguitarist94alex@gmail.com>
2018-03-24 13:47:38 +0300

    Create main.py

---
+++ bot62/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import telebot
+
+token = "LaLaLaLaLa"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['Start'])
+def on_message(message):
+  bot.send_message(message.chat.id, 'Здравствуйте! Я совместо создание группы\n'
+                                     'ТМП-61 bot61 v0.0. Что я умею делать:\n'
+                                     '-- blablabla')
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
